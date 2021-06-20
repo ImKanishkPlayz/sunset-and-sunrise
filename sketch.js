ce041dfb63bf909044941caffb74f2a3c4d4d025
ImKanishkPlayz <74292805+ImKanishkPlayz@users.noreply.github.com>
2021-06-20 15:18:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var backgroundImg;
 
 var bg ;
+var time = 0;
 
 function preload() {
     getBackgroundImg();
@@ -26,38 +27,64 @@ function draw(){
         background(backgroundImg);
     } 
 
+    
+
     Engine.update(engine);
 
     // write code to display time in correct format here
-
+     noStroke();
+    textSize(35)
+    fill("white")
+    text("Time: " + time, width-300, 50)
 
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Australia/Brisbane");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
-    if(hour >= 06 && hour <= 07 || hour >= 08 && hour <= 09 || hour >= 10 && hour <= 11 || hour >= 12 && hour <= 13 || hour >= 14 && hour <= 15 || hour >= 16 && hour <= 17 || hour >= 18 && hour <= 19 || hour >= 20 && hour <= 21 || hour >= 22 && hour <= 23 || hour >= 24 && hour <= 01 || hour >= 02 && hour <= 03){
+    if(hour >= 04 && hour <= 05){
 
-        bg = "sunrise1.png";
+        bg = "sunrise1.png";423251346123565222
+    }
+    else if(hour >= 06 && hour <= 07 ){
+
+        bg = "sunrise2.png";
+    }
+    else if(hour >= 07 && hour <= 08){
+
+        bg = "sunrise3.png";
+    }
+    else if(hour >= 08 && hour <= 09){
+
+        bg = "sunrise4.png";
+    }
+    else if(hour >= 10 && hour <= 12){
+
+        bg = "sunrise5.png";
+    }
+    else if(hour >= 13 && hour <= 15){
+
+        bg = "sunrise6.png";
+    }
+    else if(hour >= 16 && hour <= 17){
+
+        bg = "sunset7.png";
+    }
+    else if(hour >= 17 && hour <= 18){
+
+        bg = "sunset8.png";
+    }
+    
+    else if(hour >=19 && hour <= 22){
+
+        bg = "sunset11.png";
     }
     else{
-        bg = "sunrise2.png";
-        bg = "sunrise3.png";
-        bg = "sunrise4.png";
-        bg = "sunrise5.png";
-        bg = "sunrise6.png";
-        bg = "sunset7.png";
-        bg = "sunset8.png";
-        bg = "sunset10.png";
-        bg = "sunset11.png";
         bg = "sunset12.png";
     }
-
-    
-    
-
+        
     backgroundImg = loadImage(bg);
     
 }
